cfde0fead80462ee54a317293f6b0f8b88ed748a
Edward Kahn <ekahn@mqplp.com>
2023-03-07 16:59:51 -0400

    we

--- v2/futures/websocket_service.go
+++ v2/futures/websocket_service.go
@@ -14,7 +14,8 @@ const (
 	baseWsMainUrl          = "wss://fstream.binance.com/ws"
 	baseWsTestnetUrl       = "wss://stream.binancefuture.com/ws"
 	// baseCombinedMainURL    = "wss://fstream.binance.com/stream?streams="
-	baseCombinedMainURL    = "wss://52.198.6.129:443/stream?streams="
+	baseCombinedMainURL    = "wss://18.181.134.197:443/stream?streams="
+	// baseCombinedMainURL    = "wss://52.198.6.129:443/stream?streams="
 	baseCombinedTestnetURL = "wss://stream.binancefuture.com/stream?streams="
 )
 
